4a49f653ea30a5769aa1527c5d2a21b4c154d5c8
negitoyo <negishitoyokazu@toyo-pc-2.local>
2019-05-08 10:10:23 +0900

    docs:農園情報追加

--- resources/views/shop/info.blade.php
+++ resources/views/shop/info.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.layout')
 @section('title', 'ねぎし農園')
 @section('content')
-<h1>農園情報</h1>
+
+<div class="col-md-6 mx-auto">
+  <div class="card bg-light">
+    <div class="card-title mx-auto mt-4">
+      <h2 class="">農園情報</h2>
+    </div>
+    <div class="card-body mx-4">
+      <dl class="border-bottom">
+        <dt>名称</dt>
+        <dd>ねぎし農園</dd>
+      </dl>
+      <dl class="border-bottom">
+        <dt>所在地</dt>
+        <dd>〒123-4567<br>
+          群馬県邑楽郡板倉町板倉1234
+        </dd>
+      </dl>
+      <dl class="border-bottom">
+        <dt>連絡先</dt>
+        <dd>【TEL】0123-45-6789<br>
+            【E-mail】negi-nouen@gmail.com<br>
+        </dd>
+      </dl>
+    </div>
+  </div>
+
+</div>
 @endsection
